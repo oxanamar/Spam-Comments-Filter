cfc24866a994af8067669ab593ea211ecfba5ab8
oxanadev <shana_bj@hotmail.com>
2024-06-05 22:48:52 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const nameOutput = document.querySelector("#nameoutput");
+const avatarOutput = document.querySelector("#avataroutput");
+const commentOutput = document.querySelector("#commentoutput");
+const button = document.querySelector("#button");
+
+function inputInfo() {
+  let name = document.querySelector("#name").value;
+  let avatar = document.querySelector("#avatar").value;
+  let comment = document.querySelector("#comment").value;
+
+  let name1 = name.trim();
+  let name2 = name1[0].toUpperCase();
+  let name3 = name1.slice(1).toLowerCase();
+  let name4 = name2 + name3;
+  console.log(name4);
+
+  let checkSpam = comment.replace(/viagra/gi, "***");
+
+  nameOutput.textContent = name4;
+  avatarOutput.src = avatar;
+  commentOutput.textContent = checkSpam;
+}
+
+button.addEventListener("click", inputInfo);
